3b7e5dcc6eaa93605b2380c0d3a12fd2283c5664
junwankimm <jw1510@yonsei.ac.kr>
2023-11-29 07:55:44 +0000

    added hrn, requirements, install.sh

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
 Codes for audio driven 3D face generation
+
+1. run install.sh file (Not verified yet, so check files and dependencies)
+2. place keys.ini at ./talk_module/LLM/keys.ini
+3. change line 438 of modelscope.models.cv.face_reconstruction.utils file from 
+
+    original : os.path.join(save_dir, save_name + '.jpg'), mesh['texture_map']) 
+    modified : os.path.join(save_dir, save_name + '.png'), mesh['texture_map'])
+
+    since texturemap in pymeshlab only supports png files
---
+++ talk_module/vits/install.sh
@@ -0,0 +1,16 @@
+# export PATH="/usr/local/cuda-12.1/bin:$PATH"
+# export LD_LIBRARY_PATH="/usr/local/cuda-12.1/lib64:$LD_LIBRARY_PATH"
+pip install -r requirements.txt # Works on CUDA 12.1 & Pytorch 2.1.1
+gdown 1eXHmBJdYxcvOMkkAnP887ABU0ar-hQKp -O ./face_module/MICA/data/FLAME2020.zip #FLAME2020.zip
+unzip ./face_module/MICA/data/FLAME2020.zip -d ./face_module/MICA/data/FLAME2020
+gdown 1GQ6VlVClmzuFcNEpgwk87uc_4HHTJqPy -O ./face_module/MICA/data/pretrained/mica.tar #mica.tar
+gdown 1BHR4dlltm-G9vaHKXUvV1-koP4nDYPO4 -O ./face_module/LDT/data/fac3Xmodel_head_tri.zip
+unzip ./face_module/LDT/data/fac3Xmodel_head_tri.zip -d ./face_module/LDT/data/fac3Xmodel_head_tri
+gdown 1KQZ-WGI9VDFLqgNXvJQosKVCbjTaCPqK -O ./face_module/EmoTalk_release/pretrain_model/EmoTalk.pth
+gdown 1q86w74Ygw2hNzYP9cWkeClGT5X25PvBT -O ./talk_module/vits/pretrained_ljs.pth
+gdown 11aHOlhnxzjpdWDpsz1vFDCzbeEfoIxru -O ./talk_module/vits/pretrained_vctk.pth
+sudo apt-get install ffmpeg
+sudo apt-get install python3-opencv
+sudo apt-get install espeak
+# rm ./face_module/MICA/data/FLAME2020.zip
+# rm ./face_module/LDT/data/fac3Xmodel_head_tri.zip
